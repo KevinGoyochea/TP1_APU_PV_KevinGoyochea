c6d60705ab9a7ef9fb4e624315907bf3dde97d95
kevin <kevin@LaGoat>
2024-04-05 23:22:46 -0300

    Se agrega paquete y clase para el ejercicio 1

---
+++ tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,27 @@
+package ar.edu.unju.fi.ejercicio1;
+
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Random random = new Random ();
+			
+		
+		int num1 = random.nextInt(10);
+		int num2 = random.nextInt(10);
+		int num3 = random.nextInt(10);
+		int num4 = random.nextInt(10);
+		int num5 = random.nextInt(10);
+		
+		int prom = (num1 + num2 + num3 + num4 + num5)/5;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println("el promedio es: "+prom);
+	}
+
+}
