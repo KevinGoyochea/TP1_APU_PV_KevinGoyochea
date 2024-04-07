7d4cdb5ea115646e9921607c864a07aea48f9e30
kevin <kevin@LaGoat>
2024-04-06 21:34:41 -0300

    se agrega el punto 2 y correcciones del primer punto

--- tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -8,11 +8,11 @@ public class Main {
 		Random random = new Random ();
 			
 		
-		int num1 = random.nextInt(10);
-		int num2 = random.nextInt(10);
-		int num3 = random.nextInt(10);
-		int num4 = random.nextInt(10);
-		int num5 = random.nextInt(10);
+		int num1 = random.nextInt(20);
+		int num2 = random.nextInt(20);
+		int num3 = random.nextInt(20);
+		int num4 = random.nextInt(20);
+		int num5 = random.nextInt(20);
 		
 		int prom = (num1 + num2 + num3 + num4 + num5)/5;
 		
---
+++ tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String pais = "Argentina";
+		int edad = 23;
+		float alturaEdi = 48.5F;
+		float precio = 31500.99f;
+		long nTele = 3884873601L;
+		float coseno = 0.999F;
+		
+		System.out.println("Nombre pais: "+pais);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura edificio: "+alturaEdi+"m");
+		System.out.println("Precio: $"+precio);
+		System.out.println("Numero de telefono: "+nTele);
+		System.out.println("El coseno de 0.5 es: "+coseno);
+	}
+
+}
