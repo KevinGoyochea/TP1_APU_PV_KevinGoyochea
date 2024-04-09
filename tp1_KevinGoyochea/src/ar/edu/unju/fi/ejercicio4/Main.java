4f91b5065358f7798f4fa9d7eb355b084a719bc1
kevin <kevin@LaGoat>
2024-04-09 20:29:53 -0300

    Se agrega el punto 5 y una estructura condicional al punto 4

--- tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio4/Main.java
+++ tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -5,13 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		int i, fact=1, num1 ;
-		System.out.println("Ingrese el numero que desee calcular el factorial");
+		System.out.println("Ingrese el numero entero, entre el 0 y el 10, que desee calcular el factorial");
 		int num = sc.nextInt();
 		num1=num;
-		System.out.println(num);
-		while (num!=0) {
-			fact=fact*num;
-			num=num-1;
+		if (num>=0 && num<=10) {
+			System.out.println(num);
+			while (num!=0) {
+				fact=fact*num;
+				num=num-1;
+			}
+		}else {
+			System.out.println("el valor añadido no es valido, verifique los parametros establecidos");
 		}
 		System.out.println("El factorial de "+num1+" es: "+fact);
 
---
+++ tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio5;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in) ;
+		System.out.println("Escriba un numero entero, entre el 1 y el 9, que desee saber su tabla de multiplicacion: ");
+		int num = sc.nextInt();
+		if (num>0 && num<10) {
+			for (int i=0;i<11;i++) {
+				int multi = num * i;
+				System.out.println(num+" x "+ i + " = "+ multi);
+			
+			}
+		}else {
+			System.out.println("El valor agregado no esta entre los parametros establecidos anteriormente");
+		}
+		
+		
+		
+		//	System.out.println(num);
+
+	}
+
+}
