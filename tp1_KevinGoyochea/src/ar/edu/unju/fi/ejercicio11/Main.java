383c344f6aa6ade45f040e3e319f26af277568dd
kevin <kevin@LaGoat>
2024-04-16 14:42:49 -0300

    Se agrega el punto 11

---
+++ tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio11;
+public class Main {
+    public static void main(String[] args) {
+    
+    int j=40;
+    int i;
+    for (i=2;i!=42;i++,j--) {
+    	System.out.print(j*i+" ");		
+    	}
+    }
+   }
