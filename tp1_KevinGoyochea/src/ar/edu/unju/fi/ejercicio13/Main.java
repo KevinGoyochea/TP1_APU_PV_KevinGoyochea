5352856c6da120b045f3e9d8517a9a259940c7ea
kevin <kevin@LaGoat>
2024-04-19 23:02:46 -0300

    correciones del ejercicio 13

--- tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio13/Main.java
+++ tp1_KevinGoyochea/src/ar/edu/unju/fi/ejercicio13/Main.java
@@ -1,23 +1,19 @@
 package ar.edu.unju.fi.ejercicio13;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner (System.in);
-		List lista = new ArrayList();
-		int cantidad=0;
-		while( cantidad!= 8) {
-			System.out.println("Ingrese un valora para agregar al array");
-			lista.add(scanner.nextInt());
-			cantidad++;
-		}
+		int [] array = new int[8];
 		
-		for(int i = 0;i<lista.size();i++) {
-			System.out.println("Valor: "+lista.get(i)+" indice: "+i);
+		
+		for(int i = 0;i<array.length;i++) {
+			System.out.println("ingrese valor: ");
+			array[i]=scanner.nextInt();
 		}
+		for(int i = 0;i<array.length;i++) {
+			System.out.println("Valor "+array[i]+" Indice: "+i);
+		}	
 	}
-
 }
